eb25cc3f2891a3213b11f2ba10ab532e2e6f471b
admin <admin@87ea6b0e-88cb-4cdd-95e9-9c88242927a5>
2009-05-10 02:52:30 +0000

    all links will auto bind along with forms, added loading image top-left, known auto-bind issue with IE8, jquery-1.3.2.min.js bug
    git-svn-id: http://repos.svn.fusionticket.org/fusionticket/trunk@197 87ea6b0e-88cb-4cdd-95e9-9c88242927a5

--- pos/scripts/pos.jquery.ajax.js
+++ pos/scripts/pos.jquery.ajax.js
@@ -1,4 +1,5 @@
  $(document).ready(function(){
+ 	
 	var bindForms = function(){
 		$("form").submit(function(){
 			$(this).ajaxSubmit({
@@ -9,30 +10,30 @@
 			return false;
 		});
 	}
+	var bindLinks = function(){
+		//$("a:not([href^='http'])").click(function () { //does not work after rebind in ie8.
+		$("a").click(function () {
+	    	var url = $(this).attr('href');
+		   		$.ajax({
+		        	type: "GET",
+		        	url: url,
+		        	cache:false,
+		        	success: function(html){
+		        		$("#right").html(html);
+		        	}
+		   		});
+    			return false;
+		});
+	}
 	
 	bindForms();
+	bindLinks();
 	
 	$("#btn_home").click(function(){
-		$("#right").load("index.php?action=home");
+		$("#right").load("index.php?action=home");		
 	});
 	$("#btn_order").click(function(){
 		$("#right").load("index.php?action=calendar");
-	});
- 	
- 	$('.link').live("click",function () { 
-    	// Your code here. You should be able to get the href variable and
-    	// do your ajax request based on it. Something like:
-    	var url = $(this).attr('href');
-   		$.ajax({
-        	type: "GET",
-        	url: url,
-        	cache:false,
-        	success: function(html){
-        		$("#right").html(html);
-        	}
-    	});
-    	return false; // You need to return false so the link
-				// doesn't actually fire.
 	});
 	
 	$().ajaxSend(function(evt, request, settings){
@@ -42,5 +43,6 @@
 	$().ajaxStop(function(evt, request, settings){  
 		$(".loading").fadeOut("fast");  
 		bindForms();
+		bindLinks();
 	});
  });
