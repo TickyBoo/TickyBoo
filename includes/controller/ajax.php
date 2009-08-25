99e9f8f2cb438ecb562e0dd0db0fad16eed4e0b0
NielsNL <NielsNL@87ea6b0e-88cb-4cdd-95e9-9c88242927a5>
2009-08-25 07:51:27 +0000

    - changed the 'includes/web' folder name into 'includes/controller'| commit 2
    git-svn-id: http://repos.svn.fusionticket.org/fusionticket/trunk@426 87ea6b0e-88cb-4cdd-95e9-9c88242927a5

---
+++ includes/controller/ajax.php
@@ -0,0 +1,87 @@
+<?php
+/**
+%%%copyright%%%
+ *
+ * FusionTicket - ticket reservation system
+ *  Copyright (C) 2007-2009 Christopher Jenkins, Niels, Lou. All rights reserved.
+ *
+ * Original Design:
+ *	phpMyTicket - ticket reservation system
+ * 	Copyright (C) 2004-2005 Anna Putrino, Stanislav Chachkov. All rights reserved.
+ *
+ * This file is part of FusionTicket.
+ *
+ * This file may be distributed and/or modified under the terms of the
+ * "GNU General Public License" version 3 as published by the Free
+ * Software Foundation and appearing in the file LICENSE included in
+ * the packaging of this file.
+ *
+ * This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING
+ * THE WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE.
+ *
+ * Any links or references to Fusion Ticket must be left in under our licensing agreement.
+ *
+ * By USING this file you are agreeing to the above terms of use. REMOVING this licence does NOT
+ * remove your obligation to the terms of use.
+ *
+ * The "GNU General Public License" (GPL) is available at
+ * http://www.gnu.org/copyleft/gpl.html.
+ *
+ * Contact help@fusionticket.com if any conditions of this licencing isn't
+ * clear to you.
+ */
+
+/**
+ * AJAJ will return JSON only!
+ * 
+ * The class will follow strict rules and load the settings to see if a session is present 
+ * if not then will return false with a bad request status
+ * 
+ * JSON Requests should allways use json_encode(mixed, JSON_FORCE_OBJECT)
+ * Its allways good practice to turn the var into an object as 
+ * JSON is 'Object Notifaction'
+ * 
+ */
+
+$fond = 0;
+
+if($_REQUEST['pos']) {
+  require_once ( 'pos_template.php');
+} else {
+  require_once ( 'template.php');
+}
+
+
+if(isset($_SERVER['HTTP_X_REQUESTED_WITH']) && strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) == 'xmlhttprequest'){
+
+	if($_REQUEST['pos'] == true){
+		if (isset($_REQUEST['action'])) {
+			$r = $_REQUEST;
+		    $action = clean($_REQUEST['action']); //need to be cleaned so no false data can be included.
+		    require_once("classes/pos_ajax.php");
+		    $PosAjax = new PosAjax($r,$action);
+		    $result = $PosAjax->callAction();
+		}
+		if(!$result){
+		    $object = array("status" => false, "reason" => 'Missing action request');
+		    echo json_encode($object);
+		}
+	}elseif($_REQUEST['admin'] == true){
+
+	}elseif($_REQUEST['test'] == true){
+		$object = array("status"=>true,
+			"reason"=>"",
+			"request"=>"<h1>Test JSON Data</h1>"
+			);
+		echo json_encode($object);
+	}else{
+		$object = array("status"=>false,
+		"reason"=>'Incorrect Request');
+		echo json_encode($object);
+	}
+}else{
+	header("Status: 400");
+	echo "This is for AJAX / AJAJ / AJAH requests only, please go else where.";
+}
+?>
---
+++ includes/controller/template.php
@@ -0,0 +1,91 @@
+<?php
+/**
+%%%copyright%%%
+ *
+ * FusionTicket - ticket reservation system
+ *  Copyright (C) 2007-2009 Christopher Jenkins, Niels, Lou. All rights reserved.
+ *
+ * Original Design:
+ *	phpMyTicket - ticket reservation system
+ * 	Copyright (C) 2004-2005 Anna Putrino, Stanislav Chachkov. All rights reserved.
+ *
+ * This file is part of FusionTicket.
+ *
+ * This file may be distributed and/or modified under the terms of the
+ * "GNU General Public License" version 3 as published by the Free
+ * Software Foundation and appearing in the file LICENSE included in
+ * the packaging of this file.
+ *
+ * This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING
+ * THE WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE.
+ *
+ * Any links or references to Fusion Ticket must be left in under our licensing agreement.
+ *
+ * By USING this file you are agreeing to the above terms of use. REMOVING this licence does NOT
+ * remove your obligation to the terms of use.
+ *
+ * The "GNU General Public License" (GPL) is available at
+ * http://www.gnu.org/copyleft/gpl.html.
+ *
+ * Contact help@fusionticket.com if any conditions of this licencing isn't
+ * clear to you.
+ */
+
+require_once("includes/config/init_shop.php");
+
+/*/Check page is secure
+if($_SERVER['SERVER_PORT'] != 443 || $_SERVER['HTTPS'] !== "on") {
+$url = $_SHOP->root_secured.$_SERVER['REQUEST_URI'];
+echo "<script>window.location.href='$url';</script>"; exit;
+//header("Location: https://"$_SHOP->root_secured.$_SERVER['SCRIPT_NAME']);exit;}
+}
+//remove the www. to stop certificate errors.
+if(("https://".$_SERVER['SERVER_NAME']."/") != ($_SHOP->root_secured)) {
+$url = $_SHOP->root_secured.$_SERVER['REQUEST_URI'];
+echo "<script>window.location.href='$url';</script>"; exit;
+}*/
+
+require_once ( 'smarty/Smarty.class.php');
+require_once ( 'classes/MyCart_Smarty.php');
+require_once ( 'classes/User_Smarty.php');
+require_once ( 'classes/Order_Smarty.php');
+require_once ( 'classes/gui_smarty.php');
+require_once ( 'classes/Update_Smarty.php');
+require_once("config/init.php");
+
+
+global $_SHOP;
+
+
+$smarty = new Smarty;
+$_SHOP->smarty = $smarty;
+
+$gui    = new Gui_smarty($smarty);
+$cart   = new MyCart_Smarty($smarty);
+$user   = new User_Smarty($smarty);
+$order  = new Order_Smarty($smarty);
+$update = new Update_Smarty($smarty);
+
+$smarty->assign('_SHOP_root', $_SHOP->root);
+$smarty->assign('_SHOP_root_secured', $_SHOP->root_secured);
+$smarty->assign('_SHOP_lang', $_SHOP->lang);
+$smarty->assign('_SHOP_theme', $_SHOP->theme_dir);
+
+$smarty->assign('organizer_currency', $_SHOP->organizer_data->organizer_currency);
+$smarty->assign('organizer', $_SHOP->organizer_data);
+
+
+$smarty->template_dir = array($_SHOP->tpl_dir.'web'.DS.'custum'.DS, $_SHOP->tpl_dir.'web'.DS);
+$smarty->compile_dir  = $_SHOP->tmp_dir; // . '/web/templates_c/';
+$smarty->compile_id   = 'webshop_'.$_SHOP->lang;
+$smarty->cache_dir    = $_SHOP->tmp_dir;// . '/web/cache/';
+$smarty->config_dir   = $_SHOP->includes_dir . 'lang'.DS;
+
+$smarty->plugins_dir  = array("plugins", $_SHOP->includes_dir . "shop_plugins".DS);
+
+If ($fond) {
+  $smarty->display($fond . '.tpl');
+}
+
+?>
