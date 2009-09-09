4a4851fb89a07ffabaf8caa2232204614c2d4f21
admin <admin@87ea6b0e-88cb-4cdd-95e9-9c88242927a5>
2009-09-09 21:46:37 +0000

    -- applied a fix to json encode for pre 5.2.1
    git-svn-id: http://repos.svn.fusionticket.org/fusionticket/trunk@458 87ea6b0e-88cb-4cdd-95e9-9c88242927a5

--- includes/controller/ajax.php
+++ includes/controller/ajax.php
@@ -54,8 +54,7 @@ if($_REQUEST['pos']) {
 
 if (!function_exists('json_encode')) {
 	
-	function json_encode($arr) {
-	    if(function_exists('json_encode')) return json_encode($arr); //Lastest versions of PHP already has this functionality.
+	function array2json($arr) {
 	    $parts = array();
 	    $is_list = false;
 	
@@ -96,51 +95,9 @@ if (!function_exists('json_encode')) {
 	    return '{' . $json . '}';//Return associative JSON 
 	}
 	
-	
-	/* Old Function 
-  function json_encode($a=false)
-  {
-    if (is_null($a)) return 'null';
-    if ($a === false) return 'false';
-    if ($a === true) return 'true';
-    if (is_scalar($a))
-    {
-      if (is_float($a))
-      {
-        // Always use "." for floats.
-        return floatval(str_replace(",", ".", strval($a)));
-      }
-
-      if (is_string($a))
-      {
-        static $jsonReplaces = array(array("\\", "/", "\n", "\t", "\r", "\b", "\f", '"'), array('\\\\', '\\/', '\\n', '\\t', '\\r', '\\b', '\\f', '\"'));
-        return '"' . str_replace($jsonReplaces[0], $jsonReplaces[1], $a) . '"';
-      }
-      else
-        return $a;
-    }
-    $isList = true;
-    for ($i = 0, reset($a); $i < count($a); $i++, next($a))
-    {
-      if (key($a) !== $i)
-      {
-        $isList = false;
-        break;
-      }
-    }
-    $result = array();
-    if ($isList)
-    {
-      foreach ($a as $v) $result[] = json_encode($v);
-      return '[' . join(',', $result) . ']';
-    }
-    else
-    {
-      foreach ($a as $k => $v) $result[] = json_encode($k).':'.json_encode($v);
-      return '{' . join(',', $result) . '}';
-    }
-  }
-  */
+	function json_encode($arr){
+		return array2json($arr);
+	}
 }
 
 
