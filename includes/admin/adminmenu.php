3deb99dca9dbe02088b5b2f77d5695738fd1a5b5
NielsNL <NielsNL@87ea6b0e-88cb-4cdd-95e9-9c88242927a5>
2009-10-13 21:30:05 +0000

    - added a patch by ebruse so the current admin menu is shown selected
    git-svn-id: http://repos.svn.fusionticket.org/fusionticket/trunk@587 87ea6b0e-88cb-4cdd-95e9-9c88242927a5

--- includes/admin/adminmenu.php
+++ includes/admin/adminmenu.php
@@ -39,20 +39,44 @@ class MenuAdmin extends AUIComponent {
     function draw ()
     {
         global $_SHOP;
-        echo "<center>
-        <table width='".$this->width."' class='menu_admin' cellspacing='1' >
-        <tr><td  class='menu_admin_title'>" . con('administration') . "</td></tr>
+		// Get the current filename
+		$current_script_name=$_SERVER["SCRIPT_NAME"];
+		$exploded_script_name=explode("/",$current_script_name);
+		$current_file_name = $exploded_script_name[count($exploded_script_name) - 1];
+		
+		// Specify menu items in an array
+		//  "file name" 		=> "text_define"    (text define from /includes/lang/site_XX.inc)
+		$menu_items = array (
+			"index.php" 		=> "index_admin",
+			"view_users.php"	=> "users_admin",
+			"view_event.php"	=> "event_admin",
+			"view_stats.php"	=> "stats",
+			"view_order.php"	=> "order_admin",
+			"view_template.php"	=> "template_admin",
+			"view_handling.php"	=> "payment_admin",
+			"view_search.php"	=> "search_order",
+			"view_impexp.php"	=> "imp_export_admin",
+			"view_garbage.php"	=> "garbage"
+		);
 
-        <tr><td  class='menu_admin_item'><a href='index.php'       class='menu_admin_link'>" . con('index_admin') . "</a></td></tr>
-        <tr><td  class='menu_admin_item'><a href='view_users.php'  class='menu_admin_link'>" . con('users_admin') . "</a></td></tr>
- 		    <tr><td  class='menu_admin_item'><a href='view_event.php'  class='menu_admin_link'>" . con('event_admin') . "</a></td></tr>
-        <tr><td  class='menu_admin_item'><a href='view_stats.php'  class='menu_admin_link'>" . con('stats') . "</a></td></tr>
-        <tr><td  class='menu_admin_item'><a href='view_order.php'  class='menu_admin_link'>" . con('order_admin') . "</a></td></tr>
-        <tr><td  class='menu_admin_item'><a href='view_template.php'    class='menu_admin_link'>" . con('template_admin') . "</a></td></tr>
-        <tr><td  class='menu_admin_item'><a href='view_handling.php'    class='menu_admin_link'>" . con('payment_admin') . "</a></td></tr>
-        <tr><td  class='menu_admin_item'><a href='view_search.php' class='menu_admin_link'>" . con('search_order') . "</a></td></tr>
-        <tr><td  class='menu_admin_item'><a href='view_impexp.php'      class='menu_admin_link'>" . con('imp_export_admin') . "</a></td></tr>
-        <tr><td  class='menu_admin_item'><a href='view_garbage.php'     class='menu_admin_link'>" . con('garbage') . "</a></td></tr>";
+		// Begin drawing the menu table
+		echo "<center>
+        <table width='".$this->width."' class='menu_admin' cellspacing='1' >
+        <tr><td  class='menu_admin_title'>" . con('administration') . "</td></tr>";
+		
+		// Loop through the menu item array and put the'menu_admin_link_selected'-class on the linkt to current file
+		foreach($menu_items as $link => $text){ 
+			echo "<tr><td  class='menu_admin_item'><a href={$link} ";
+			if ($link==$current_file_name){
+				echo "class='menu_admin_link_selected'>";
+			} else {
+				echo "class='menu_admin_link'>";
+			}
+			echo con($text);
+			echo "</a></td></tr>";
+		}
+		// Continue and close the menu table below
+
         /*
        if($_SHOP->is_admin){
          echo "<tr><td  class='menu_admin_item'><a href='$_SHOP->php_myadmin' class='menu_admin_link'>PHP MyAdmin</a></td></tr>";
