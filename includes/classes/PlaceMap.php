9631f40133b592cacf2ad477f53dc6871eac5bc7
admin <admin@87ea6b0e-88cb-4cdd-95e9-9c88242927a5>
2009-11-20 17:28:14 +0000

    -Fixed PlaceMap.php
    git-svn-id: http://repos.svn.fusionticket.org/fusionticket/trunk@707 87ea6b0e-88cb-4cdd-95e9-9c88242927a5

--- includes/classes/PlaceMap.php
+++ includes/classes/PlaceMap.php
@@ -14,7 +14,7 @@
  * This file may be distributed and/or modified under the terms of the
  * "GNU General Public License" version 3 as published by the Free
  * Software Foundation and appearing in the file LICENSE included in
- * the packaging of this file.
+ * the packaging of this file.p
  *
  * This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING
  * THE WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR
@@ -209,7 +209,7 @@ class PlaceMap Extends Model {
           }
         }
       }
-      if (ShopDB::commit('copied Placmap to event: '.$event_id){
+      if (ShopDB::commit('copied Placmap to event: '.$event_id)){
         return $new_id;
       }
     }
