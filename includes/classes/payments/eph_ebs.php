9fb9215182f43255f8bc41e48d0a04e52fe6f268
NielsNL <NielsNL@87ea6b0e-88cb-4cdd-95e9-9c88242927a5>
2009-12-23 18:44:01 +0000

    - Updated the ebs eph
    git-svn-id: http://repos.svn.fusionticket.org/fusionticket/trunk@847 87ea6b0e-88cb-4cdd-95e9-9c88242927a5

--- includes/classes/payments/eph_ebs.php
+++ includes/classes/payments/eph_ebs.php
@@ -31,7 +31,7 @@
  * Contact help@fusionticket.com if any conditions of this licencing isn't
  * clear to you.
  */
-if (!defined('ft_check')) {die('System intrusion ');}
+//if (!defined('ft_check')) {die('System intrusion ');}
 
 require_once('classes/Payment.php');
 
@@ -42,10 +42,10 @@ class EPH_ebs extends payment{
 
 	function admin_view (){
     return "{gui->view name='pm_ebs_accountid'}".
-           "{gui->view name='pm_ebs_seckey'}".    
+           "{gui->view name='pm_ebs_seckey'}".
            "{gui->view name='pm_paypal_test'}";
 	}
-	
+
   function admin_form (){
     return "{gui->input name='pm_ebs_accountid'}".
            "{gui->input name='pm_ebs_seckey'}".
@@ -69,13 +69,16 @@ class EPH_ebs extends payment{
     return "
       <form  method='post' action='https://secure.ebs.in/pg/ma/sale/pay/' name='frmTransaction' id='frmTransaction'>
         <input name='account_id' type='hidden' value='{$this->pm_ebs_accountid}' />
+        {literal}
         <input name='return_url' type='hidden' value='".$_SHOP->root_secured. 'checkout_accept.php?'.$order->EncodeSecureCode()."&DR={DR}' />
+        {/literal}
         <input name='mode' type='hidden' value='{$ebs_mode}'  />
         <input name='reference_no' type='hidden' value='{$order->order_id}'  />
         <input name='amount' type='hidden' value='".sprintf("%01.2F", ($order->order_total_price))."' />
-        <input name='description' type='hidden' value='{$order->order_description()}'  /> 
+        <input name='description' type='hidden' value='{$order->order_description()}'  />
         <input name='name' type='hidden' value='{$order->user_firstname} {$order->user_lastname}'  />
-        <input name='city' type='hidden'  value='{$order->user_city}' />    
+        <input name='address' type='hidden'  value='{$order->user_address}' />
+        <input name='city' type='hidden'  value='{$order->user_city}' />
         <input name='state' type='hidden' value='{$order->user_state}'  />
         <input name='postal_code' type='hidden'  value='{$order->user_zip}' />
         <input name='country' type='hidden'  value='IND' /> <!-- {$order->user_country} -->
@@ -84,24 +87,23 @@ class EPH_ebs extends payment{
         <input name='ship_name' type='hidden'  value='{$order->user_firstname} {$order->user_lastname}' />
         <input name='ship_address' type='hidden'  value='{$order->user_address}' />
         <input name='ship_city' type='hidden' value='{$order->user_city}'  />
-        <input name='ship_state' type='hidden'  value='{$order->user_state}' />    
-        <input name='ship_postal_code' type='hidden'  value='{$order->user_zip}' />    
+        <input name='ship_state' type='hidden'  value='{$order->user_state}' />
+        <input name='ship_postal_code' type='hidden'  value='{$order->user_zip}' />
         <input name='ship_country'  type='hidden'  value='IND' > <!-- {$order->user_country} -->
         <input name='ship_phone' type='hidden' value='{$order->user_phone}'/>
-        
+
         <div align='right'>
-          <input type='submit' value='{!pay!}' name='submitted' alt='{!paypal_pay!}' >
+          <input type='submit' value='{!pay!}' name='submitted' alt='{!ebs_pay!}' >
         </div>
       </form>";
   }
 
   function on_return(&$order, $result){
-
-    if(isset($_GET['DR'])) {
-      require('.\ebs\Rc43.php');
+     if(isset($_GET['DR'])) {
+      require('ebs'.DS.'Rc43.php');
       $DR = preg_replace("/\s/","+",$_GET['DR']);
-      $rc4 = new Crypt_RC4($this->pm_ebs_seckey);
       $QueryString = base64_decode($DR);
+      $rc4 = new Crypt_RC4($this->pm_ebs_seckey);
       $rc4->decrypt($QueryString);
       $QueryString = split('&',$QueryString);
 
@@ -111,19 +113,19 @@ class EPH_ebs extends payment{
         $response[$param[0]] = urldecode($param[1]);
       }
     }
-  
+    //var_dump($response);
     if ($response['PaymentID']) {
-      Order::set_payment_id($order->order_id,'paypal:'.$response['PaymentID']);
+      Order::set_payment_id($order->order_id,'ebs:'.$response['PaymentID']);
     }
-    if ($response['ResponceCode']===0) {
-      $order->set_payment_status('paid');
+    if ($response['ResponseCode']==0) {
+      $order->set_payment_status('payed');
       return array('approved'=>true,
                    'transaction_id'=>$response['PaymentID'],
-                   'response'=> $response['ResponceMessage']);
+                   'response'=> $response['ResponseMessage']);
     } else {
       return array('approved'=>false,
                    'transaction_id'=>$response['PaymentID'],
-                   'response'=> $response['ResponceCode'].') '. $response['ResponceMessage']);
+                   'response'=> $response['ResponseCode'].': '. $response['ResponseMessage']);
     }
   }
 }
