b2bc594e7124aaff96e6901a6e5b2d7cf92f7c11
NielsNL <NielsNL@87ea6b0e-88cb-4cdd-95e9-9c88242927a5>
2010-01-10 17:52:10 +0000

    git-svn-id: http://repos.svn.fusionticket.org/fusionticket/trunk@902 87ea6b0e-88cb-4cdd-95e9-9c88242927a5

--- includes/classes/basics.php
+++ includes/classes/basics.php
@@ -274,7 +274,7 @@ function empt(&$arg , $default=null){
         $addcon = "<?php\ndefine('{$name}','{$name}');\n?>\n";
         file_put_contents($_SHOP->langfile, $addcon, FILE_APPEND);
         define($name,$name);
-      }
+      } else echo "****|";
       return "****{$name}****";
     }
   }
